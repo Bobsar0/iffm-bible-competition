d5fd7d1d68dde919ba8eca9bc65fe527b486a53e
Steve Onyeneke <bobsarglobal@gmail.com>
2022-03-27 11:52:36 +0100

    align eliminated competitor's widget evenly

--- lib/views/pages/competition_page/components/eliminated_competitors.dart
+++ lib/views/pages/competition_page/components/eliminated_competitors.dart
@@ -13,7 +13,7 @@ class EliminatedParticipants extends StatelessWidget {
     List<Participant> eliminatedParticipants =
         context.watch<ParticipantController>().eliminatedParticipants.toList();
 
-    return eliminatedParticipants.length == 0
+    return eliminatedParticipants.isEmpty
         ? Container()
         : Column(
             children: [
@@ -21,43 +21,45 @@ class EliminatedParticipants extends StatelessWidget {
               const Text('Eliminated',
                   style: TextStyle(fontWeight: FontWeight.bold)),
               ListView.separated(
-                padding: const EdgeInsets.all(8),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 0),
                 shrinkWrap: true,
                 itemCount: eliminatedParticipants.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10),
-                    height: 40,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(eliminatedParticipants[index].name!,
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(eliminatedParticipants[index].name!,
                             style: const TextStyle(
                                 fontSize: 14,
                                 decoration: TextDecoration.lineThrough),
                             overflow: TextOverflow.ellipsis,
                             maxLines: 2),
-                        IconButton(
-                          icon: const Icon(Icons.add_circle_outline),
-                          color: Colors.green,
-                          onPressed: () {
-                            ModalDialog(
-                              context: context,
-                              title: 'Confirm',
-                              message:
-                                  'Are you sure you want to bring back ${eliminatedParticipants[index].name}?',
-                              actionBtnText: 'Yes',
-                              cancelBtnText: 'Cancel',
-                              onActionBtnPressed: () => context
-                                  .read<ParticipantController>()
-                                  .recoverParticipant(
-                                      eliminatedParticipants[index]),
-                            );
-                          },
-                        )
-                      ],
-                    ),
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.add_circle_outline,
+                          size: 20,
+                        ),
+                        color: Colors.green,
+                        onPressed: () {
+                          ModalDialog(
+                            context: context,
+                            title: 'Confirm',
+                            message:
+                                'Are you sure you want to bring back ${eliminatedParticipants[index].name}?',
+                            actionBtnText: 'Yes',
+                            cancelBtnText: 'Cancel',
+                            onActionBtnPressed: () => context
+                                .read<ParticipantController>()
+                                .recoverParticipant(
+                                    eliminatedParticipants[index]),
+                          );
+                        },
+                      )
+                    ],
                   );
                 },
                 separatorBuilder: (BuildContext context, int index) =>
